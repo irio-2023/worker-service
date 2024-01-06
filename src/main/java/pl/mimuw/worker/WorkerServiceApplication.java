1cff0299645901f9823a2494439b37a8aa8b6223
Hubert Michalski <43937184+xvisox@users.noreply.github.com>
2024-01-06 19:16:10 +0100

    Monitor task schema (#1)
    * add monitor task schema
    
    * add monitor task logic
    
    * add testcontainers emulator
    
    * fix

--- src/main/java/pl/mimuw/worker/WorkerServiceApplication.java
+++ src/main/java/pl/mimuw/worker/WorkerServiceApplication.java
@@ -2,13 +2,15 @@ package pl.mimuw.worker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class TemplateServiceApplication {
+@EnableScheduling
+public class WorkerServiceApplication {
 
     // Swagger UI: http://localhost:8080/swagger-ui.html
     public static void main(String[] args) {
-        SpringApplication.run(TemplateServiceApplication.class, args);
+        SpringApplication.run(WorkerServiceApplication.class, args);
     }
 
 }
--- src/test/java/pl/mimuw/worker/AbstractIT.java
+++ src/test/java/pl/mimuw/worker/AbstractIT.java
@@ -1,16 +1,45 @@
 package pl.mimuw.worker;
 
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.core.NoCredentialsProvider;
 import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PubSubEmulatorContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @DirtiesContext
 @AutoConfigureObservability
+@Testcontainers
 @ActiveProfiles("it-test")
-@SpringBootTest(classes = TemplateServiceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = WorkerServiceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class AbstractIT {
+
+    @TestConfiguration
+    static class PubSubEmulatorConfiguration {
+        @Bean
+        CredentialsProvider googleCredentials() {
+            return NoCredentialsProvider.create();
+        }
+    }
+
+    @Container
+    static final PubSubEmulatorContainer pubSubEmulator = new PubSubEmulatorContainer(
+            DockerImageName.parse("gcr.io/google.com/cloudsdktool/google-cloud-cli:441.0.0-emulators")
+    );
+
+    @DynamicPropertySource
+    static void emulatorProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.cloud.gcp.pubsub.emulator-host", pubSubEmulator::getEmulatorEndpoint);
+    }
 
     @LocalServerPort
     protected Integer port;
